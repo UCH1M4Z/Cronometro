c4b605565251f7ebca35257145efa1addc9bd3f6
UCH1M4Z <uchimajuanjo@gmail.com>
2025-04-02 18:05:07 -0500

    Update ChromaCounterJS.js

--- ChromaCounterJS.js
+++ ChromaCounterJS.js
@@ -1,16 +1,44 @@
-let time
-let isRunning = false
-let seconds =0
-let minutes =0
+let time = null;
+let isRunning = false;
+let seconds =0;
+let minutes =0;
 
-const timeDisplay =document.getElementById('time')
+const timeDisplay =document.getElementsByClassName('time')
 const startStopButton =document.getElementById('starStop')
 const resetButton =document.getElementById('reset')
 const pauseButton =document.getElementById('pause')
 
+window.onload = function(){
+    var interval = window.setInterval(function(){
+        incrementarTiempo()
+    }, 1000)
+}
 
 function actualizartime(){
-    let displayMinutos= minutos <10 ?'0'+minutes:minutes
+    let displayMinutes= minutes <10 ?'0'+minutes:minutes
     let displaySeconds= seconds <10 ?'0'+seconds:seconds
-    timeDisplay.textContent =`${displayMinutes}:${displaySeconds}`;
+    document.getElementsByClassName('time').textContent =`${displayMinutes}:${displaySeconds}`;
+    
+    console.log(document.getElementsByClassName('time').textContent)
 }
+
+
+function incrementarTiempo() {
+    seconds++;
+    if (seconds === 60) {
+      minutes++;
+      seconds = 0;
+    }
+    actualizartime();
+  }
+
+
+function startStop() {
+    if (!isRunning) {
+      isRunning = true;
+      time = setInterval(incrementarTiempo, 1000);
+    } else {
+      isRunning = false;
+      clearInterval(time);
+    }
+  }
